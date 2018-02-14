5ba98aad51df66cb76f27e0f3f2d192a4160c4de
Alaethia <truthcharis@gmail.com>
2018-02-14 14:03:15 -0600

    added voice synthesis

--- server/public/scripts/controllers/alphabet.controller.js
+++ server/public/scripts/controllers/alphabet.controller.js
@@ -24,16 +24,8 @@ self.previousLetter = function () {
         self.index = 25;
     }//end if
 }//go to previous letter 
-self.sayMsg = function(message){
-    let msg = new SpeechSynthesisUtterance(message);
-    var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[17]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
-    msg.voiceURI = 'native';
-    msg.volume = 2; // 0 to 1
-    msg.rate = .8; // 0.1 to 10
-    msg.pitch = 1; //0 to 2      
-    msg.lang = 'en-US';
-         window.speechSynthesis.speak(msg);
- }//end message function
+self.sayMsg = function(msg) {
+    AppService.sayMsg(msg)
+}//end call to service for speech utterance 
 }]);//end controller
 
--- server/public/scripts/service/app.service.js
+++ server/public/scripts/service/app.service.js
@@ -20,5 +20,16 @@ myApp.service('AppService',['$http', function($http) {
             self.NumbersData.list = result.data;
         })
     }//end get alphabet data
+    self.sayMsg = function(message){
+        let msg = new SpeechSynthesisUtterance(message);
+        var voices = window.speechSynthesis.getVoices();
+        msg.voice = voices[7]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
+        msg.voiceURI = 'native';
+        msg.volume = 2; // 0 to 1
+        msg.rate = .8; // 0.1 to 10
+        msg.pitch = 1; //0 to 2      
+        msg.lang = 'en-US';
+        window.speechSynthesis.speak(msg);
+        }//end message function
 
 }]);//end service
