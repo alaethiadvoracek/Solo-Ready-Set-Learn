b3a527706db1475a6a7d8c1f494431b334b8fb86
Alaethia <truthcharis@gmail.com>
2018-02-15 19:54:49 -0600

    changed voice path

--- server/public/scripts/service/app.service.js
+++ server/public/scripts/service/app.service.js
@@ -33,7 +33,7 @@ myApp.service('AppService',['$http', function($http) {
     self.sayMsg = function(message){
         let msg = new SpeechSynthesisUtterance(message);
         var voices = window.speechSynthesis.getVoices();
-        msg.voice = voices[7]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
+        msg.voice = voices[25]; // Note: voices are unpredictable at the moment, still experimental tech. 7 is the clearest male, 17 the clearest female, 25 is good
         msg.voiceURI = 'native';
         msg.volume = 2; // 0 to 1
         msg.rate = .8; // 0.1 to 10
